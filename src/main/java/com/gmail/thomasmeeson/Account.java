354838b5fa00df268252b3074c093eebb79f4063
Tom <13624619+meesont@users.noreply.github.com>
2019-05-12 13:35:17 +0100

    Attempt to add an input function

--- src/main/java/com/gmail/thomasmeeson/Account.java
+++ src/main/java/com/gmail/thomasmeeson/Account.java
@@ -1,15 +1,28 @@
 package com.gmail.thomasmeeson;
 
+import com.google.gson.annotations.Expose;
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Account {
 
+    @Expose
     private double balance;
+    @Expose
     private String forename, surname, title;
+    @Expose
     private UUID accountId;
+    @Expose
     private Date creationDate;
 
+    /**
+     * A constructor for the account class, when the balance is known
+     * @param forename The forename of the account holder
+     * @param surname The surname of the account holder
+     * @param title The title of the account holder
+     * @param balance The starting balance of the account
+     */
     public Account(String forename, String surname, String title, double balance) {
         this.balance = balance;
         this.forename = forename;
@@ -20,6 +33,12 @@ public class Account {
         this.creationDate = new Date();
     }
 
+    /**
+     * A constructor for the account class, when the balance is unknown
+     * @param forename The forename of the account holder
+     * @param surname The surname of the account holder
+     * @param title The title of the account holder
+     */
     public Account(String forename, String surname, String title) {
         this.forename = forename;
         this.surname = surname;
@@ -68,5 +87,10 @@ public class Account {
 
     public Date getCreationDate() {
         return creationDate;
+    }
+
+    public void addBalance(double balance) {
+        double newBal = this.getBalance() + balance;
+        this.balance = newBal;
     }
 }
--- src/main/java/com/gmail/thomasmeeson/Core.java
+++ src/main/java/com/gmail/thomasmeeson/Core.java
@@ -3,9 +3,7 @@ package com.gmail.thomasmeeson;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 
 public class Core {
 
@@ -13,9 +11,13 @@ public class Core {
 
 
         Account account = new Account("Tom", "Meeson", "Mr", 0);
+        Account account1 = new Account("Sam", "Smith", "Sir", 100000);
+        Account[] accounts = {account,account1};
 
-        writeOutput(account, "output");
 
+        //writeOutput(accounts, "output");
+
+        readInput("output");
     }
 
     public static void writeOutput(Object obj, String fileName) {
@@ -33,6 +35,39 @@ public class Core {
         } catch (IOException e) {
             System.out.println("IO Exception occurred");
             e.printStackTrace();
+        }
+    }
+
+    public static void readInput(String fileName) {
+
+        Gson gson = new Gson();
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(fileName + ".json"));
+
+            Result result = gson.fromJson(br, Result.class);
+
+            if(result != null) {
+                for(Account a : result.getAccounts()) {
+                    System.out.println(
+                            "Account Details\n\n"
+                                    + "Surname: " + a.getSurname() + "\n"
+                                    + "Forename: " + a.getForename() + "\n"
+                                    + "UUID: " + a.getAccountId());
+                    System.out.println("----------------------");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File: " + fileName + ".json was not found");
+        } finally {
+            if(br != null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
